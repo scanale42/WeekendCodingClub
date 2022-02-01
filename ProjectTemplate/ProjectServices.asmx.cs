4a0b0ff612d8fa2f2501073e35ac44ecc118bf3a
Scott Canale <scanale42@gmail.com>
2022-01-31 19:35:02 -0700

    Added the logon webservice

--- ProjectTemplate/ProjectServices.asmx.cs
+++ ProjectTemplate/ProjectServices.asmx.cs
@@ -62,5 +62,50 @@ namespace ProjectTemplate
 				return "Something went wrong, please check your credentials and db name and try again.  Error: "+e.Message;
 			}
 		}
-	}
+
+
+        [WebMethod]
+        public bool LogOn(string uid, string pass)
+        {
+            //LOGIC: pass the parameters into the database to see if an account
+            //with these credentials exist.  If it does, then return true.  If
+            //it doesn't, then return false
+
+            //we return this flag to tell them if they logged in or not
+            bool success = false;
+
+            //our connection string comes from our web.config file like we talked about earlier
+            string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
+            //here's our query.  A basic select with nothing fancy.  Note the parameters that begin with @
+            string sqlSelect = "SELECT id FROM accounts WHERE userid=@idValue and pass=@passValue";
+
+            //set up our connection object to be ready to use our connection string
+            MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
+            //set up our command object to use our connection, and our query
+            MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+            //tell our command to replace the @parameters with real values
+            //we decode them because they came to us via the web so they were encoded
+            //for transmission (funky characters escaped, mostly)
+            sqlCommand.Parameters.AddWithValue("@idValue", HttpUtility.UrlDecode(uid));
+            sqlCommand.Parameters.AddWithValue("@passValue", HttpUtility.UrlDecode(pass));
+
+            //a data adapter acts like a bridge between our command object and 
+            //the data we are trying to get back and put in a table object
+            MySqlDataAdapter sqlDa = new MySqlDataAdapter(sqlCommand);
+            //here's the table we want to fill with the results from our query
+            DataTable sqlDt = new DataTable();
+            //here we go filling it!
+            sqlDa.Fill(sqlDt);
+            //check to see if any rows were returned.  If they were, it means it's 
+            //a legit account
+            if (sqlDt.Rows.Count > 0)
+            {
+                //flip our flag to true so we return a value that lets them know they're logged in
+                success = true;
+            }
+            //return the result!
+            return success;
+        }
+    }
 }
