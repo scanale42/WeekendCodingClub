44f8adeae3368690eba36147c5574a4185a86208
Scott Canale <scanale42@gmail.com>
2022-02-02 20:39:03 -0700

    Added post suggestion functionality in posts page

--- ProjectTemplate/ProjectServices.asmx.cs
+++ ProjectTemplate/ProjectServices.asmx.cs
@@ -64,7 +64,7 @@ namespace ProjectTemplate
 		}
 
 
-        [WebMethod]
+        [WebMethod(EnableSession = true)] //NOTICE: gotta enable session on each individual method
         public bool LogOn(string uid, string pass)
         {
             //LOGIC: pass the parameters into the database to see if an account
@@ -101,11 +101,58 @@ namespace ProjectTemplate
             //a legit account
             if (sqlDt.Rows.Count > 0)
             {
-                //flip our flag to true so we return a value that lets them know they're logged in
+                //if we found an account, store the id and admin status in the session
+                //so we can check those values later on other method calls to see if they 
+                //are 1) logged in at all, and 2) and admin or not
+                Session["id"] = sqlDt.Rows[0]["id"];
+                //Session["admin"] = sqlDt.Rows[0]["admin"];
                 success = true;
             }
             //return the result!
             return success;
         }
+
+        //EXAMPLE OF AN INSERT QUERY WITH PARAMS PASSED IN.  BONUS GETTING THE INSERTED ID FROM THE DB!
+        [WebMethod(EnableSession = true)]
+        public void NewPost(string title, string desc)
+        {
+            int userID = Convert.ToInt32(Session["id"]);
+            string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
+            //the only thing fancy about this query is SELECT LAST_INSERT_ID() at the end.  All that
+            //does is tell mySql server to return the primary key of the last inserted row.
+            //string sqlSelect = "insert into suggestions(title, desc, submitter, approved, status) " +
+            //    "values(@titleValue, @descValue, 1, 0, 0); SELECT LAST_INSERT_ID();";
+            string sqlSelect = "insert into suggestions (title, `desc`, submitter, approved, status) " +
+            "values(@titleValue, @descValue, @userID, 0, 0); SELECT LAST_INSERT_ID();";
+
+            MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
+            MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+            sqlCommand.Parameters.AddWithValue("@titleValue", HttpUtility.UrlDecode(title));
+            sqlCommand.Parameters.AddWithValue("@descValue", HttpUtility.UrlDecode(desc));
+            sqlCommand.Parameters.AddWithValue("@userID", userID);
+            //sqlCommand.Parameters.AddWithValue("@submitterValue", HttpUtility.UrlDecode(submitter));
+
+            //this time, we're not using a data adapter to fill a data table.  We're just
+            //opening the connection, telling our command to "executescalar" which says basically
+            //execute the query and just hand me back the number the query returns (the ID, remember?).
+            //don't forget to close the connection!
+            sqlConnection.Open();
+            //we're using a try/catch so that if the query errors out we can handle it gracefully
+            //by closing the connection and moving on
+           try
+            {
+                int postID = Convert.ToInt32(sqlCommand.ExecuteScalar());
+                //here, you could use this accountID for additional queries regarding
+                //the requested account.  Really this is just an example to show you
+                //a query where you get the primary key of the inserted row back from
+                //the database!
+            }
+            catch (Exception e)
+            {
+            }
+            sqlConnection.Close();
+        }
+
     }
 }
